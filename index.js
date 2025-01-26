84e15eab344b11368425389c2772ec4e930b96aa
nishigandha1435 <satabdisaikia39@gmail.com>
2025-01-26 11:59:21 -0600

    calculator

---
+++ index.js
@@ -0,0 +1,21 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+         if (buttonText == 'AC') {
+            screenValue = '0';
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
